8687d277d12da09d6544f014fa64a8c2b5b982ed
Andrew Shewmaker <shewa@lanl.gov>
2015-11-13 21:45:08 -0800

    compiled, but untested

--- tcp_relentless.c
+++ tcp_relentless.c
@@ -29,6 +29,7 @@
 #include <net/tcp.h>
 
 #define RELENTLESS_MAX_MARK 1024U
+#define RELENTLESS_MAX_URGENCY 2048U
 
 static unsigned int markthresh __read_mostly = 174;
 module_param(markthresh, uint, 0644);
@@ -48,6 +49,10 @@ static unsigned int debug_src __read_mostly = 167772162; // 10.0.0.2
 module_param(debug_src, int, 0644);
 MODULE_PARM_DESC(debug_src, "Source IP address to match for debugging (0=all)");
 
+static bool deadline_aware  __read_mostly = false;
+module_param(deadline_aware, bool, 0644);
+MODULE_PARM_DESC(deadline_aware, "enable deadline awareness, defaults false");
+
 /* Relentless structure */
 struct relentless {
 	u32 save_cwnd;     /* saved cwnd from before disorder or recovery */
@@ -56,6 +61,9 @@ struct relentless {
 	u32 rtt_min;
 	u32 rtt_thresh;
 	u32 rtt_cwnd;      /* cwnd scaled by 1024 */
+	u32 maxw_at_rtt_min;
+	s32 packets_left;
+	ktime_t deadline;
 	bool debug;
 };
 
@@ -72,12 +80,16 @@ inline static void relentless_init(struct sock *sk)
 	ca->rtt_min = USEC_PER_SEC;
 	ca->rtt_cwnd = tp->snd_cwnd << 10U;
 
+	ca->maxw_at_rtt_min = tp->snd_cwnd;
+	ca->packets_left = 0;
+	ca->deadline = ktime_set(0, 0);
+
         ca->debug = false;
-	pr_info("dctcp: saddr=%u\n", saddr);
+	pr_info("relentless: saddr=%u\n", saddr);
         if (debug_port == 0 || ((ntohs(inet->inet_dport) == debug_port) && saddr == debug_src))
                 ca->debug = true;
 
-	pr_info("relentless init: rtt_cwnd=%u\n", ca->rtt_cwnd);
+	pr_info("relentless: rtt_cwnd=%u\n", ca->rtt_cwnd);
 }
 
 void relentless_cong_avoid(struct sock *sk, u32 ack, u32 acked)
@@ -136,6 +148,141 @@ static void relentless_event(struct sock *sk, enum tcp_ca_event event)
 	}
 }
 
+s64 relentless_urgency(struct sock *sk)
+{
+	const struct tcp_sock *tp = tcp_sk(sk);
+	struct relentless *ca = inet_csk_ca(sk);
+	s64 urgency = 0, time_to_complete = 0;
+	s64 dl_us = ktime_to_us(ca->deadline);
+
+	if (dl_us <= 0)
+		return urgency;
+
+	time_to_complete = (ca->rtt_thresh) *
+			   (ca->packets_left - tp->packets_out) * 4 / (3 * tp->snd_cwnd);
+	urgency = RELENTLESS_MAX_URGENCY * time_to_complete / dl_us;
+	urgency = clamp(urgency, (s64) 0U, (s64) RELENTLESS_MAX_URGENCY);
+
+	pr_info_ratelimited("urgency=%u\n", (u32) urgency);
+	return urgency;
+}
+
+void relentless_check_deadline(struct sock *sk, u32 rtt)
+{
+	struct relentless *ca = inet_csk_ca(sk);
+	//struct tcp_sock *tp = tcp_sk(sk);
+	u64 p = 0;
+	ktime_t now = ktime_get();
+
+	/* only change deadline info after the previous deadline has passed */
+	if (!ktime_equal(ca->deadline, ktime_set(0, 0)) && ktime_before(ca->deadline, now))
+		return;
+
+	switch (sk->sk_priority) {
+		//u32 load, w_load, rtt_load, rtt_thresh, quantum, w_per_p;
+		//u32 load, quantum, w_per_p;
+		case TC_PRIO_BESTEFFORT:
+/* let's get other cases working first
+			if (ca->rtt_min < INIGO_SUSPECT_RTT) {
+				pr_warn_ratelimited("rtt_min is suspect. not calculating besteffort\n");
+				return;
+			}
+			// doesn't work so well ... way too high
+			//rtt_thresh = ca->rtt_min + (markthresh * ca->rtt_min / INIGO_MAX_MARK);
+			//rtt_load = INIGO_LOAD_SCALE * (rtt - rtt_thresh) / rtt_thresh;
+			load = ca->maxw_at_rtt_min / tp->snd_cwnd;
+			quantum = NSEC_PER_SEC / 2;
+			p = max(1U, load * quantum);
+			w_per_p = p / NSEC_PER_USEC / ca->rtt_min;
+			ca->packets_left = w_per_p * ca->maxw_at_rtt_min;
+			pr_info_ratelimited("load=%u, p=%llu, w_per_p=%u, packets_left=%u, peak bw(Mbps)=%u, share bw(Mbps)=%u\n",
+					     load, p, w_per_p, ca->packets_left,
+					     8*1500*ca->maxw_at_rtt_min / ca->rtt_min,
+					     8*1500*ca->packets_left / (u32) (p / NSEC_PER_USEC));
+			break;
+ */
+		case TC_PRIO_FILLER:
+			/* deadline unaware filler */
+			return;
+		case TC_PRIO_BULK:
+			/* approximately 15MB/2s */
+			ca->packets_left = 10000;
+			p = NSEC_PER_SEC * 2;
+			break;
+		case 3:
+			/* approximately 75MB/2s */
+			ca->packets_left = 50000;
+			p = NSEC_PER_SEC * 2;
+			break;
+		case TC_PRIO_INTERACTIVE_BULK:
+			/* approximately 15KB/0.1s */
+			ca->packets_left = 10;
+			p = NSEC_PER_SEC / 10;
+			break;
+		case 5:
+			/* approximately 30KB/0.1s */
+			ca->packets_left = 20;
+			p = NSEC_PER_SEC / 10;
+			break;
+		case TC_PRIO_INTERACTIVE:
+			/* approximately 3KB/0.1s */
+			ca->packets_left = 2;
+			p = NSEC_PER_SEC / 10;
+			break;
+		case TC_PRIO_CONTROL:
+			/* approximately 3KB/0.05s */
+			ca->packets_left = 2;
+			p = NSEC_PER_SEC / 20;
+			break;
+		case 8:
+			/* from Brandt RTTS03 paper fig 7*/
+			// util = 25 / 100;
+			p = NSEC_PER_SEC * 2 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 25 / (ca->rtt_min * 100);
+			break;
+		case 9:
+			/* from Brandt RTTS03 paper fig 7*/
+			// util = 30 / 100;
+			p = NSEC_PER_SEC * 5 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 30 / (ca->rtt_min * 100);
+			break;
+		case 10:
+			/* from Brandt RTTS03 paper fig 7*/
+			// util = 35 / 100;
+			p = NSEC_PER_SEC;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 35 / (ca->rtt_min * 100);
+			break;
+		case 11:
+			/* from Brandt RTTS03 paper fig 8*/
+			// util = 20 / 100;
+			p = NSEC_PER_SEC * 2 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 20 / (ca->rtt_min * 100);
+			break;
+		case 12:
+			/* from Brandt RTTS03 paper fig 8*/
+			// util = 60 / 100;
+			p = NSEC_PER_SEC;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 60 / (ca->rtt_min * 100);
+			break;
+		case 13:
+			/* from Brandt RTTS03 paper fig 8*/
+			// util = 40 / 100;
+			p = NSEC_PER_SEC * 5 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 40 / (ca->rtt_min * 100);
+			break;
+		case 14:
+			p = NSEC_PER_SEC * 5 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 70 / (ca->rtt_min * 100);
+			break;
+		case TC_PRIO_MAX:
+			p = NSEC_PER_SEC * 5 / 10;
+			ca->packets_left = p * ca->maxw_at_rtt_min * 90 / (ca->rtt_min * 100);
+			break;
+	}
+
+	ca->deadline = ktime_add_us(now, p);
+}
+
 static void relentless_pkts_acked(struct sock *sk, u32 num_acked, s32 rtt)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
@@ -158,8 +305,14 @@ static void relentless_pkts_acked(struct sock *sk, u32 num_acked, s32 rtt)
 
 	/* Mimic DCTCP ECN marking threshhold of approximately 0.17*BDP */
 	if (r > ca->rtt_thresh) {
-		ca->rtt_cwnd -= (num_acked << 6U);
-		ca->rtt_cwnd = max(ca->rtt_cwnd, (2U << 10U));
+		bool backoff = tp->snd_cwnd >= tp->snd_ssthresh && prandom_u32_max(RELENTLESS_MAX_URGENCY) > relentless_urgency(sk);
+
+		pr_info_ratelimited("relentless: backoff=%u\n", backoff);
+
+		if (backoff) {
+			ca->rtt_cwnd -= (num_acked << 6U);
+			ca->rtt_cwnd = max(ca->rtt_cwnd, (2U << 10U));
+		}
 
 		if (ca->debug)
 			pr_info_ratelimited("relentless backoff: rtt_min=%u, rtt_thresh=%u, rtt=%u, rtt_cwnd=%u, cwnd=%u, ssthresh=%u\n",
@@ -175,8 +328,14 @@ static void relentless_pkts_acked(struct sock *sk, u32 num_acked, s32 rtt)
 					ca->rtt_min, ca->rtt_thresh, (u32)r, ca->rtt_cwnd, tp->snd_cwnd, tp->snd_ssthresh);
 		}
 	} else {
+		ca->maxw_at_rtt_min = max(ca->maxw_at_rtt_min, tp->snd_cwnd);
 		ca->rtt_cwnd += (1 << 10U);
 		tp->snd_cwnd = (ca->rtt_cwnd >> 10U);
+	}
+
+	if (deadline_aware && (tp->snd_cwnd > tp->snd_ssthresh)) {
+		relentless_check_deadline(sk, (u32) rtt);
+		ca->packets_left -= num_acked;
 	}
 }
 
